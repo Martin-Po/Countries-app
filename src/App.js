dc8aab4bfd00e5e05439512f473a02c7af9122bb
Martin-Po <85139462+Martin-Po@users.noreply.github.com>
2023-12-14 15:24:38 -0300

    commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, CssBaseline } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+import AppBar from './components/AppBar'
+import Footer from './components/Footer'
+import CountriesCards from './components/CountriesCards'
+import FilterSection from './components/FilterSection'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import { initializeCountries } from './reducers/countriesReducer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit2 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const dispatch = useDispatch()
+    const languaje = useSelector((state) => state.languaje)
+
+    useEffect(() => {
+        // Dispatch action to initialize countries
+        dispatch(initializeCountries())
+    }, [dispatch])
+
+    // Fetch countries from the Redux store
+    const countries = useSelector((state) => state.countries)
+
+    console.log(countries)
+    useEffect(() => {
+        if (countries && countries.length > 0) {
+            initializeListedCountries()
+        }
+    }, [countries])
+
+    const [listedCountries, setlistedCountries] = useState([])
+    const [shownCountries, setshownCountries] = useState([])
+    const [filteredContinent, setfilteredContinent] = useState([])
+    const [filteredCountries, setfilteredCountries] = useState([])
+    const [activeFilter, setactiveFilter] = useState('')
+
+    const initializeListedCountries = () => {
+        setlistedCountries([])
+        countries.forEach((country) => {
+            const newcountry = {
+                name: [
+                    { lang: 'ENG', text: country.name.common },
+                    { lang: 'ESP', text: country.translations['spa'].common },
+                ],
+                population: country.population,
+                continents: country.continents,
+                capital: country.capital,
+                flag: country.flags.png,
+            }
+            setlistedCountries((prevListedCountries) =>
+                prevListedCountries.concat(newcountry)
+            )
+            setshownCountries(listedCountries)
+        })
+    }
+
+    const filterCountries = (newFilter) => {
+        setactiveFilter(newFilter)
+        console.log('se esta ejecutando')
+        console.log(newFilter)
+        if (countries && countries.length > 0) {
+            console.log('aca entro')
+            if (newFilter === '') {
+                console.log('aca entro1')
+
+                setshownCountries(listedCountries)
+            } else {
+                if (shownCountries.length === 0)  setshownCountries(listedCountries)
+
+                console.log('aca entro2')
+                console.log(
+                    shownCountries.filter((country) =>
+    country.name
+        .filter((text) => text.lang === languaje)
+        .some((text) => text.text.toLowerCase().includes(newFilter.toLowerCase()))
+)
+                )
+
+                setshownCountries(
+                    shownCountries.filter((country) =>
+    country.name
+        .filter((text) => text.lang === languaje)
+        .some((text) => text.text.toLowerCase().includes(newFilter.toLowerCase()))
+)
+                )
+            }
+            console.log('shownCountries')
+            console.log(shownCountries)
+            console.log('shownCountries2')
+        }
+    }
+
+console.log('activefilter');
+console.log(activeFilter);
+console.log('activefilter2')
+
+    return (
+        <div style={{ minHeight: '100vh' }}>
+            <CssBaseline />
+            <AppBar />
+            <Grid
+                container
+                spacing={{ xs: 0, md: 4 }}
+                sx={{
+                    marginLeft: 'auto',
+                    marginRight: 'auto',
+                    width: '1280px',
+                    marginTop: '56px',
+                    display:'flex',
+                    flexDirection:'column',
+
+                    '@media (max-width:1300px)': {
+                        width: '992px',
+                    },
+                    '@media (max-width:1024px)': {
+                        width: '100%',
+                    },
+                }}
+            >
+                <FilterSection
+                    listedCountries={listedCountries}
+                    filterCountries={filterCountries}
+                />
+                <CountriesCards countries={listedCountries.filter((country) =>
+    country.name
+        .filter((text) => text.lang === languaje)
+        .some((text) => text.text.toLowerCase().includes(activeFilter.toLowerCase()))
+)} />
+                <Box></Box>
+            </Grid>
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
